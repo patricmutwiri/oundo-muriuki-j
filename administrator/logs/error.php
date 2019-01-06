a1cabcdb26f45e91ae930b6e5d3babb881eb8431
Patrick Mutwiri <patwiri@gmail.com>
2019-01-06 18:55:30 +0300

    fc

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-01-14 17:29:12 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-01-14T17:29:12+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-01-23T05:41:46+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2018-05-02T18:46:15+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ tmp/template_copy_5ae4480e2fa89/js/mutwiri.js
@@ -0,0 +1,13 @@
+	/*
+		* @author/Developer: Patrick Mutwiri
+		* Email: patwiri@gmail.com
+		* @link/GIT: https://github.com/patricmutwiri
+		* @link/Twitter: https://twitter.com/patric_mutwiri
+		* Call: +254727542899
+		* @link/Site: http://patric.xyz
+	*/
+	jQuery(function(){
+		//I go in here | it's what I do | 
+		jQuery('.sp-simpleportfolio-item.shuffle-item.filtered').trigger('hover');
+
+	});
